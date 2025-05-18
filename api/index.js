659773448f185add6f0f2444a68eb884a34cd170
V V V N Uday Aditya <67990361+adityavvvn@users.noreply.github.com>
2025-05-18 15:44:17 +0530

    update2

--- api/index.js
+++ api/index.js
@@ -21,7 +21,6 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
-
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log('✅ Connected to MongoDB');
